1f6ff2ff32925da88852960d1cb3ba87a3c69339
Qing Sun <sunqingshine@163.com>
2020-02-10 14:30:37 +0100

    add choice

--- reviews.py
+++ reviews.py
@@ -14,3 +14,9 @@ for d in data:  #把清单中的每一个都依次拿出来
 	sum_len = sum_len + len(d) #把每一个长度加起来
 
 print('average len', sum_len/len(data))
+
+new = []  #建立新清单
+for d in data:    #先写for loop去读取资料，行程字串
+	if len(d)<100:
+		new.append(d)
+print('in tatol there are', len(new), 'reviews')
